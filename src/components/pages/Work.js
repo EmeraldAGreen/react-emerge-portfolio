1692a02880cdcf9c43c793af6a504ca897d1fba9
Emerge <egreengirl1@gmail.com>
2022-05-19 13:38:09 -0600

    added styles to Work.js and imported png images

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import ShyGuyFinance from '../assets/ShyGuy-Finance.png';
+import WeatherDashboard from '../assets/weather-dashboard.png'
 
 export default function Work() {
   return (
-    <div>
+    <div id="deployed-projects" className="deployed-projects">
       <h1>My Work</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+          <div className="row">
+            <div className="col-sm-6">
+                <div className="card border-dark rounded">
+                    <div className="card-body">
+                        <h5 className="card-title">ShyGuyFinance </h5>
+                          <img src={ShyGuyFinance} className="card-img-top" alt="..."/>
+                          <a href="https://mtnbiker72.github.io/Project1-Finance/" className="btn btn-dark">GitHub Pages</a>
+                          <a href="https://github.com/mtnbiker72/Project1-Finance" className="btn btn-dark">Github Repo</a>
+                    </div>
+                  </div>
+                </div>
+              <div className="col-sm-6">
+                  <div className="card border-dark">
+                      <div className="card-body">
+                          <h5 className="card-title">Weather Dashboard</h5>
+                          <img src={WeatherDashboard} className="card-img-top" alt="..."/>
+                          <a href="https://emeraldagreen.github.io/city-weather-forecast/" className="btn btn-dark">Github Pages</a>
+                          <a href="https://github.com/EmeraldAGreen/city-weather-forecast" className="btn btn-dark">Github Repo</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
     </div>
   );
 }

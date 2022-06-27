f716b3940ce4b66009968d3662f8018677689243
Emerge <egreengirl1@gmail.com>
2022-06-26 22:00:11 -0500

    mobile responsive contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,6 +3,8 @@ import emailjs from '@emailjs/browser';
 
 import { validateEmail } from '../../utils/helpers';
 
+import { Container, Row} from 'react-bootstrap';
+
 function Contact() {
    // set initial form state
   const initialState = 
@@ -59,42 +61,46 @@ function Contact() {
  
 
   return (
-    <section className="container-center-horizontal">
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button type="submit">Submit</button>
-      </form>
+    <section>
+      <Container>
+        <Row className="container-center-horizontal">
+          <form id="contact-form" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                name="name"
+                defaultValue={name}
+                onBlur={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email address:</label>
+              <input
+                type="email"
+                name="email"
+                defaultValue={email}
+                onBlur={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="message">Message:</label>
+              <textarea
+                name="message"
+                rows="5"
+                defaultValue={message}
+                onBlur={handleChange}
+              />
+            </div>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+            <button type="submit">Submit</button>
+          </form>
+        </Row>
+      </Container>
     </section>
   );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import pdf from "../../assets/pdfs/Resume_2022.pdf";
-import { Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Card, ListGroup } from 'react-bootstrap';
 
 function Resume() {
   return (
@@ -9,38 +9,42 @@ function Resume() {
         Download my <button className="button-download"><a href={pdf} download="Resume_2022.pdf">resume</a> </button>
         </p>
       <div className="my-2 container-center-horizontal">
-      <Card>
-        <Card.Header>
-        <h3>Front-end Proficiencies</h3>
-        </Card.Header>
-        <Card.Body>
-        <ListGroup className="skills">
-          <ListGroup.Item>HTML</ListGroup.Item>
-          <ListGroup.Item>CSS</ListGroup.Item>
-          <ListGroup.Item>JavaScript</ListGroup.Item>
-          <ListGroup.Item>jQuery</ListGroup.Item>
-          <ListGroup.Item>responsive design</ListGroup.Item>
-          <ListGroup.Item>React</ListGroup.Item>
-          <ListGroup.Item>Bootstrap</ListGroup.Item>
-        </ListGroup>
-        </Card.Body>.
-      </Card>
-      <Card>
-        <Card.Header>
-        <h3>Back-end Proficiencies</h3>
-        </Card.Header>
-        <Card.Body>
-        <ListGroup className="skills">
-          <ListGroup.Item>APIs</ListGroup.Item>
-          <ListGroup.Item>Node</ListGroup.Item>
-          <ListGroup.Item>Express</ListGroup.Item>
-          <ListGroup.Item>MySQL, Sequelize</ListGroup.Item>
-          <ListGroup.Item>MongoDB, Mongoose</ListGroup.Item>
-          <ListGroup.Item>REST</ListGroup.Item>
-          <ListGroup.Item>GraphQL</ListGroup.Item>
-        </ListGroup>
-        </Card.Body>.
-      </Card>
+      <Container>
+        <Row>
+          <Card>
+            <Card.Header>
+            <h3>Front-end Proficiencies</h3>
+            </Card.Header>
+            <Card.Body>
+            <ListGroup className="skills">
+              <ListGroup.Item>HTML</ListGroup.Item>
+              <ListGroup.Item>CSS</ListGroup.Item>
+              <ListGroup.Item>JavaScript</ListGroup.Item>
+              <ListGroup.Item>jQuery</ListGroup.Item>
+              <ListGroup.Item>responsive design</ListGroup.Item>
+              <ListGroup.Item>React</ListGroup.Item>
+              <ListGroup.Item>Bootstrap</ListGroup.Item>
+            </ListGroup>
+            </Card.Body>.
+          </Card>
+          <Card>
+            <Card.Header>
+            <h3>Back-end Proficiencies</h3>
+            </Card.Header>
+            <Card.Body>
+            <ListGroup className="skills">
+              <ListGroup.Item>APIs</ListGroup.Item>
+              <ListGroup.Item>Node</ListGroup.Item>
+              <ListGroup.Item>Express</ListGroup.Item>
+              <ListGroup.Item>MySQL, Sequelize</ListGroup.Item>
+              <ListGroup.Item>MongoDB, Mongoose</ListGroup.Item>
+              <ListGroup.Item>REST</ListGroup.Item>
+              <ListGroup.Item>GraphQL</ListGroup.Item>
+            </ListGroup>
+            </Card.Body>.
+          </Card>
+        </Row>
+      </Container>
       </div>
     </section>
   );
